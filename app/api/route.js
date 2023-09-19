8e0b401bf4f058522af6c11bba9eb334d62dc47f
ekkis <e@arix.com>
2023-09-19 12:55:29 -0700

    proper query parsing

--- app/api/route.js
+++ app/api/route.js
@@ -1,11 +1,13 @@
-import { NextResponse } from 'next/server';
- 
 export const runtime = 'edge'; // 'nodejs' is the default
- 
+
+import { NextResponse } from 'next/server';
+const jsp = require('js-prototype-lib')
+jsp.install('string')
+
 export function GET(request) {
 	return NextResponse.json({
 		body: request.body,
-		query: request.nextUrl.search,
+		query: request.nextUrl.search.substr(1).keyval('=', '&'),
 		cookies: request.cookies.getAll(),
 		referrer: request.referrer
 	}, {
