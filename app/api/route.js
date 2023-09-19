7671769e7cf8f32540bef72037df4442f46cfdaf
ekkis <e@arix.com>
2023-09-18 18:53:29 -0700

    added referrer

--- app/api/route.js
+++ app/api/route.js
@@ -3,14 +3,12 @@ import { NextResponse } from 'next/server';
 export const runtime = 'edge'; // 'nodejs' is the default
  
 export function GET(request) {
-  return NextResponse.json(
-    {
-      body: request.body,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
+	return NextResponse.json({
+		body: request.body,
+		query: request.nextUrl.search,
+		cookies: request.cookies.getAll(),
+		referrer: request.referrer
+	}, {
+		status: 200,
+	})
 }
